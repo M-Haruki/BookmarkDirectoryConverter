eb7e32ef6b1b954f5de69d4c031b8d6fa9412c2d
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2025-05-25 01:39:24 +0900

    ブックマークのエクスポートを実装

--- popup/main.js
+++ popup/main.js
@@ -2,31 +2,45 @@ function addEventListeners() {
     document.getElementById("btn-export").addEventListener("click", exportData);
 }
 
-function exportData() {
-    // Function to export data
-    console.log("Exporting data...");
-
-    browser.bookmarks.getTree().then((bookmarks) => {
-        console.log(JSON.stringify(bookmarks, null, 2));
-    });
-    var zip = new JSZip();
-    zip.file("Hello.txt", "Hello World\n");
-    let sub = zip.folder("sub");
-    sub.file("Subfile.txt", "This is a subfile\n");
-    sub.file("Subfile2.txt", "This is another subfile\n");
-    zip.generateAsync({ type: "blob" }).then((content) => downloadFile(content, "bookmarks.zip", "application/zip"));
-}
-
 function downloadFile(content, filename, type) {
-    var blob = new Blob([content], { type: type });
-    var url = URL.createObjectURL(blob);
-    var a = document.createElement("a");
+    let blob = new Blob([content], { type: type });
+    let url = URL.createObjectURL(blob);
+    let a = document.createElement("a");
     a.href = url;
     a.download = filename;
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
     URL.revokeObjectURL(url);
+}
+
+function createFileData(zip, array) {
+    array.forEach((item) => {
+        // name
+        let name = item.title.replace(/[\\/:*?"<>|]/g, "_");
+        if (name.length > 255) {
+            name = name.substring(0, 255);
+        }
+        if (name === "") {
+            name = "Untitled";
+        }
+        //
+        if (item.type === "folder") {
+            let folder = zip.folder(name);
+            createFileData(folder, item.children);
+        } else if (item.type === "bookmark") {
+            let fileName = name + ".url";
+            let fileContent = `[InternetShortcut]\nURL=${item.url}\n`;
+            zip.file(fileName, fileContent);
+        }
+    });
+}
+
+async function exportData() {
+    const bookmarks = await browser.bookmarks.getTree();
+    const zip = new JSZip();
+    createFileData(zip, bookmarks);
+    zip.generateAsync({ type: "blob" }).then((content) => downloadFile(content, "bookmarks.zip", "application/zip"));
 }
 
 addEventListeners();
